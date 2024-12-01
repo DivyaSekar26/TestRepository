66352bf21aa8aa242a0d1521217e37b74c15a108
DivyaSekar26 <divya26998@gmail.com>
2024-12-01 23:23:17 +0530

    Update Solution2.java

--- JavaAssignmentTask4/src/basics/task4/Solution2.java
+++ JavaAssignmentTask4/src/basics/task4/Solution2.java
@@ -7,8 +7,8 @@ public class Solution2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int a=10;
+		Scanner sc=new Scanner(System.in);		
+		int a=sc.nextInt();
 		if(a%2==0)
 		{
 			System.out.println("Input Number is even");
